3601e7607c3b5e529bff5691d01b8e39b9965ff9
Luis Salazar <lsalazar@grupopv.mx>
2018-08-31 13:46:39 -0600

    Create ping schedule
    - Moved the ping counter to the config file.
    - Modify the host's to use them as Inline Block.
    - Modify the ping_host function so that it verifies the function 
    working_hours and depending on the response it performs the ping.
    - Add schedules to the config file.
    - Add data such as "date, host and name" to the subject and content of 
    the email.
    - Add the working_hours function with weekday and weekend schedules.

--- lib/ping.py
+++ lib/ping.py
@@ -4,9 +4,7 @@ from os import devnull
 from subprocess import call, STDOUT
 from lib.mail import sendgrid_mail
 from yaml import load
-
-# Define variables
-count = "4"
+from datetime import datetime
 
 # Define /dev/null
 FNULL = open(devnull, 'w')
@@ -20,14 +18,35 @@ def ping_hosts():
     # Analyze every host in config.yml
     config = read_config()
     for host in range(len(config["hosts"])):
-        name = config["hosts"][host].split(" => ")[0]
-        ip = config["hosts"][host].split(" => ")[1]
-        res = call(['ping', '-c', count, ip], stdout=FNULL, stderr=STDOUT)
-        if res == 0:
-            print("Ping to", name, "OK")
-        elif res == 2:
-            # 100% failed
-            print("No response from", name)
-            sendgrid_mail(name, ip)
+        name = config["hosts"][host]["name"]
+        ip = config["hosts"][host]["host"]
+        schedule = config["hosts"][host]["schedule"]
+        count = config["ping"]["count"]
+        if schedule == 'always' or (schedule == 'working hours' and working_hours(config) == 'true'):
+            res = call(['ping', '-c', str(count), ip], stdout=FNULL, stderr=STDOUT)
+            if res == 0:
+                print("Ping to", name, "OK")
+            elif res == 2:
+                # 100% failed
+                print("No response from", name)
+                sendgrid_mail(name, ip)
+            else:
+                print("ping to", name, "failed!")
+
+def working_hours(config):
+    weekdays_working_hours_start = datetime.now().replace(hour=config["working_hours"]["weekdays"]["start"]["hour"], minute=config["working_hours"]["weekdays"]["start"]["minute"], second=config["working_hours"]["weekdays"]["start"]["second"])
+    weekdays_working_hours_finish = datetime.now().replace(hour=config["working_hours"]["weekdays"]["finish"]["hour"], minute=config["working_hours"]["weekdays"]["finish"]["minute"], second=config["working_hours"]["weekdays"]["finish"]["second"])
+    weekend_working_hours_start = datetime.now().replace(hour=config["working_hours"]["weekend"]["start"]["hour"], minute=config["working_hours"]["weekend"]["start"]["minute"], second=config["working_hours"]["weekend"]["start"]["second"])
+    weekend_working_hours_finish = datetime.now().replace(hour=config["working_hours"]["weekend"]["finish"]["hour"], minute=config["working_hours"]["weekend"]["finish"]["minute"], second=config["working_hours"]["weekend"]["finish"]["second"])
+    current_time = datetime.now()
+    current_day = int(current_time.strftime('%w'))
+    if 0 < current_day and current_day < 6:
+        if weekdays_working_hours_start < current_time and current_time < weekdays_working_hours_finish:
+            return "true"
         else:
-            print("ping to", name, "failed!")
+            return "false"
+    elif current_day == 6:
+        if weekend_working_hours_start < current_time and current_time < weekend_working_hours_finish:
+            return "true"
+        else:
+            return "false"
